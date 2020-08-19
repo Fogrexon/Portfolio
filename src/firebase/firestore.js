10d17d00b8c352ff08032def13d27fae886f3496
Fogrexon <fogrexon@gmail.com>
2020-08-19 22:25:31 +0900

    firebase setting

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,22 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { firestore } from './firebase';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-const latestWork = db.collection('gallery').orderBy('updatedAt', 'asc');
+const latestWork = firestore.collection('gallery').orderBy('updatedAt', 'asc');
 const getWorkList = (maxWorkCount) => (
   maxWorkCount ? latestWork.limit(maxWorkCount).get() : latestWork.get()
 );
