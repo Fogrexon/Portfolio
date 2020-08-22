b2786108a4f4f99ce2246f88f2c527e2f8858728
Fogrexon <fogrexon@gmail.com>
2020-08-22 09:34:52 +0900

    Blogに画像アップロードテスト

--- src/blogedit/BlogEdit.js
+++ src/blogedit/BlogEdit.js
@@ -5,12 +5,31 @@ import { Redirect } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import { getBlog, updateBlog } from '../firebase/firestore';
+import { uploadBlogImage } from '../firebase/storage';
+import style from './BlogEdit.module.scss';
+
+const ImageBox = ({ filename, url }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.target.select();
+    document.execCommand('Copy');
+  };
+  return (
+    <div className={style.image_box}>
+      <img src={url} alt={filename} />
+      <Form>
+        <Form.Control onClick={handleClick} readOnly value={`![${filename}](${url})`} />
+      </Form>
+    </div>
+  );
+};
 
 export default (props) => {
   // eslint-disable-next-line react/destructuring-assignment
   const { id } = props.match.params;
   const [markdown, setMarkdown] = useState('');
   const [title, setTitle] = useState('');
+  const [images, setImages] = useState(['初期生成マン|']);
   const [submited, setSubmited] = useState(false);
 
   useEffect(
@@ -18,6 +37,7 @@ export default (props) => {
       getBlog(id).then((blog) => {
         setMarkdown(blog.content);
         setTitle(blog.title);
+        setImages(blog.images || []);
       });
     },
     [],
@@ -29,6 +49,30 @@ export default (props) => {
   const handleMarkdownChange = (e) => {
     setMarkdown(e);
   };
+  const handleDrop = (data, e) => {
+    e.preventDefault();
+    const { files } = e.dataTransfer;
+    if (!files) return;
+    const blogImageList = [];
+    for (let i = 0; i < files.length; i += 1) {
+      const file = files[i];
+      blogImageList.push(uploadBlogImage(id, file));
+    }
+
+    Promise.all(blogImageList).then((fileUrls) => {
+      const deltaImages = fileUrls.map((url, index) => (
+        `${files[index].name}|${url}`
+      ));
+      const newImages = [
+        ...images,
+        ...deltaImages,
+      ];
+      setImages(newImages);
+      updateBlog(id, {
+        images: newImages,
+      });
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     updateBlog(id, {
@@ -38,11 +82,9 @@ export default (props) => {
       setSubmited(true);
     });
   };
-
   if (submited) {
     return <Redirect to="/admin/blog" />;
   }
-
   return (
     <Container>
       <h1 className="section-title">Blog Editor</h1>
@@ -51,7 +93,24 @@ export default (props) => {
           <Form.Label>Title</Form.Label>
           <Form.Control key="title" placeholder="Title" type="text" onChange={handleTitleChange} value={title} />
         </Form.Group>
-        {markdown ? <SimpleMDE onChange={handleMarkdownChange} value={markdown} /> : ''}
+      </Form>
+      <div className={style.image_container}>
+        {images.map((fileinfo) => {
+          const infosplit = fileinfo.split('|');
+          const filename = infosplit[0];
+          const fileurl = infosplit[1];
+          return <ImageBox key={filename} filename={filename} url={fileurl} />;
+        })}
+      </div>
+      <Form>
+        <SimpleMDE
+          key={images.join(' ')}
+          onChange={handleMarkdownChange}
+          value={markdown}
+          events={{
+            drop: handleDrop,
+          }}
+        />
         <Form.Control key="update" type="submit" onClick={handleSubmit} value="Update" />
       </Form>
     </Container>
